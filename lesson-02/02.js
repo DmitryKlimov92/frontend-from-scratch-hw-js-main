d3227c60f5f7a145968eabb662ec0eb401efe8e2
DmitryKlimov92 <the.cabbagecake@gmail.com>
2025-03-07 12:14:14 +0300

    2/1 trying

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,20 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(math.random() * 6) // your code
+let dice2 = Math.floor(math.random() * 6) // your code
+let isWinningDouble = false // your code
+
+
+if (dice1 > 3) {
+    if (dice1 === dice2) {
+        isWinningDouble = true;
+        console.log('Выигрышный дубль!')
+    }
+    else console.log('Не выигрышный дубль!')
+}
+else console.log('Переброс!')
+
 
 /*
 console.log('Первый бросок: ' + dice1)
